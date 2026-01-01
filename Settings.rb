3fcbac58d713b4dd13f144f304adea24b9076faf
zhinson1238 <165225459+IDIIIK@users.noreply.github.com>
2026-01-01 17:02:34 -0600

    Add files via upload

---
+++ Settings.rb
@@ -0,0 +1,54 @@
+
+module Settings
+  LATEST_GAME_RELEASE = "6.7.2"
+
+  SHINY_POKEMON_CHANCE = 16
+  DISCORD_URL = "https://discord.com/invite/infinitefusion"
+  WIKI_URL = "https://infinitefusion.fandom.com/"
+  STARTUP_MESSAGES = ""
+  
+  CREDITS_FILE_URL = "https://infinitefusion.net/customsprites/Sprite_Credits.csv"
+
+  SPRITES_FILE_URL = "https://raw.githubusercontent.com/infinitefusion/pif-downloadables/refs/heads/master/CUSTOM_SPRITES"
+  BASE_SPRITES_FILE_URL = "https://raw.githubusercontent.com/infinitefusion/pif-downloadables/refs/heads/master/BASE_SPRITES"
+
+
+  VERSION_FILE_URL = "https://raw.githubusercontent.com/infinitefusion/infinitefusion-e18/main/Data/VERSION"
+  CUSTOM_DEX_FILE_URL = "https://raw.githubusercontent.com/infinitefusion/pokedex-entries/main/dex.json"
+
+  # CUSTOM SPRITES
+  AUTOGEN_SPRITES_REPO_URL = ""
+  CUSTOM_SPRITES_REPO_URL = ""
+  CUSTOM_SPRITES_NEW_URL = ""
+  BASE_POKEMON_SPRITES_REPO_URL = ""
+  BASE_POKEMON_ALT_SPRITES_REPO_URL = ""
+  BASE_POKEMON_ALT_SPRITES_NEW_URL = ""
+
+  BASE_POKEMON_SPRITESHEET_URL = "https://infinitefusion.net/spritesheets/spritesheets_base/"		#legacy
+  CUSTOM_FUSIONS_SPRITESHEET_URL = "https://infinitefusion.net/spritesheets/spritesheets_custom/"	#legacy
+  BASE_POKEMON_SPRITESHEET_RESIZED_URL = "https://infinitefusion.net/spritesheets_resized/spritesheets_base/"		#legacy
+  CUSTOM_FUSIONS_SPRITESHEET_RESIZED_URL = "https://infinitefusion.net/spritesheets_resized/spritesheets_custom/"	#legacy
+ 
+
+  BASE_POKEMON_SPRITESHEET_TRUE_SIZE_URL = "https://infinitefusion.net/customsprites/spritesheets/spritesheets_base/"
+  CUSTOM_FUSIONS_SPRITESHEET_TRUE_SIZE_URL = "https://infinitefusion.net/customsprites/spritesheets/spritesheets_custom/"
+
+  
+  
+  CUSTOMSPRITES_RATE_MAX_NB_REQUESTS = 15  #Nb. requests allowed in each time window
+  CUSTOMSPRITES_ENTRIES_RATE_TIME_WINDOW = 60    # In seconds
+  MAX_NB_SPRITES_TO_DOWNLOAD_AT_ONCE =5
+
+  #POKEDEX ENTRIES
+
+  AI_ENTRIES_URL = "https://infinitefusion.net/dex/"
+  AI_ENTRIES_RATE_MAX_NB_REQUESTS = 10  #Nb. requests allowed in each time window
+  AI_ENTRIES_RATE_TIME_WINDOW = 120    # In seconds
+  AI_ENTRIES_RATE_LOG_FILE = 'Data/pokedex/rate_limit.log'  # Path to the log file
+
+  #Spritepack
+  NEWEST_SPRITEPACK_MONTH = 12
+  NEWEST_SPRITEPACK_YEAR = 2025
+
+
+end
